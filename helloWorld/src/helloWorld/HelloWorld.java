28d019d9afdbe3b7e0d773c4eb8c3dd22855c6cb
Cesar Li Chen <cesarli619@gmail.com>
2022-11-13 22:47:54 -0500

    First commit, added a .gitignore file

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 123;
+		System.out.println("Hello World");
+		System.out.println(234);
+		System.out.println(num);
+	}
+
+}
