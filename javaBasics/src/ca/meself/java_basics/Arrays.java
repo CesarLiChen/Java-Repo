af0ce2521e4c5f64acf36e18cc2102ff2e89c265
Cesar Li Chen <cesarli619@gmail.com>
2023-02-03 23:25:26 -0500

    Some multi-dimensional array syntax and concepts

--- javaBasics/src/ca/meself/java_basics/Arrays.java
+++ javaBasics/src/ca/meself/java_basics/Arrays.java
@@ -27,6 +27,32 @@ public class Arrays {
 		for (String word: words) {
 			System.out.println(word + " [for each loop]");
 		}
+		
+		// Multi-dimensional arrays
+		
+		int[][] grid = {
+				{1, 2, 3},
+				{7, 8, 9},
+				{4, 5, 6}
+		};
+		
+		for (int row = 0; row < grid.length; row++) {
+			for (int column = 0; column < grid[row].length; column++) {
+				System.out.print(grid[row][column] + " ");
+			}
+		}
+		
+		System.out.println();
+		
+		System.out.println(grid[2][2]); //6
+		System.out.println(grid[0][1]); //2
+		
+		// String[][] name = new String[rows][columns]
+		String[][] moreWords = new String[2][3];
+		
+		String[][] woords = new String[2][];
+		woords[0] = {"AHHH", "RUUUN"};
+		
 	}
 
 }
