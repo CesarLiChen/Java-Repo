3c9badc4b6004f3297564ffa3721740fcb77d0f8
Cesar Li Chen <cesarli619@gmail.com>
2023-02-03 22:49:27 -0500

    Array of strings and for-each loops

--- javaBasics/src/ca/meself/java_basics/Arrays.java
+++ javaBasics/src/ca/meself/java_basics/Arrays.java
@@ -3,14 +3,29 @@ package ca.meself.java_basics;
 public class Arrays {
 
 	public static void main(String[] args) {
-		
-		int[] values = new int[3]; // Initialization		
-		
-		
-		int[] numArray = {23, 12, 45, 67};
-		
-		for(int i = 0; i < numArray.length; i++) {
+
+		int[] values = new int[3]; // Initialization
+
+		int[] numArray = { 23, 12, 45, 67 };
+
+		for (int i = 0; i < numArray.length; i++) {
 			System.out.println("This is the value at index " + i + ": " + numArray[i]);
+		}
+		
+		// Array of strings
+		String[] words = new String[5];
+		words[0] = "I";
+		words[1] = "know";
+		words[2] = "this";
+		words[3] = "isn't";
+		words[4] = "loveeee";
+		
+		for (int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+		}
+		
+		for (String word: words) {
+			System.out.println(word + " [for each loop]");
 		}
 	}
 
